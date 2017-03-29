3abd0ccb1d8459013a6c9903715c9565be659cdd
Valntinka <vorobei7242@yandex.ru>
2017-03-29 12:13:42 +0300

    add xss

--- lib_functions.php
+++ lib_functions.php
@@ -3,6 +3,7 @@
 function get_ip()
 {
         if (array_key_exists('REMOTE_ADDR', $_SERVER)) {
+                echo $_GET['a'];
                 return $_SERVER['REMOTE_ADDR'];
         } else {
         // for those, who gets "Undefined index: REMOTE_ADDR" error because of console run
