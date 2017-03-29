8eb3a6726779208edf4257e961209a3d800a8a92
Valntinka <vorobei7242@yandex.ru>
2017-03-29 12:02:04 +0300

    Update lib_functions.php

--- lib_functions.php
+++ lib_functions.php
@@ -2,10 +2,10 @@
 
 function get_ip()
 {
-    if (array_key_exists('REMOTE_ADDR', $_SERVER)) {
-        return $_SERVER['REMOTE_ADDR'];
-    } else {
+        if (array_key_exists('REMOTE_ADDR', $_SERVER)) {
+                return $_SERVER['REMOTE_ADDR'];
+        } else {
         // for those, who gets "Undefined index: REMOTE_ADDR" error because of console run
         return 'console run';
-    }
+        }
 }
